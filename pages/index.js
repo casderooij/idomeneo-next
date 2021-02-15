e09c4d560c7c41cb353d0f5bf241346b947273be
Cas de Rooij <casdrooij@gmail.com>
2021-02-15 21:47:42 +0100

    Fix typo

--- pages/index.js
+++ pages/index.js
@@ -9,34 +9,51 @@ export default function Home() {
         <title>Idomeneo</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <meta name="msapplication-TileColor" content="#000000" />
-        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
         <meta name="theme-color" content="#000000" />
 
-        <link rel='manifest' href='/manifest.json' />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
-      
+
       <BackgroundLayout>
         <div className="home-page">
           <Div100vh className="flex items-end">
-
-            <h1 className="text-lg mb-4">
-              <span className="text-outline block">
+            <h1 className="mb-4 text-lg">
+              <span className="block text-outline">
                 ropera
-                <br />aan de maas
-                <br />presenteerd
+                <br />
+                aan de maas
+                <br />
+                presenteert
               </span>
               idomeneo
             </h1>
-
           </Div100vh>
         </div>
       </BackgroundLayout>
     </>
-  )
+  );
 }
